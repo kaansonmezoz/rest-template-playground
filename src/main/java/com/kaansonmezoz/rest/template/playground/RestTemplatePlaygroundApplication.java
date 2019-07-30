c5c76b822c801aebb7937d3b7276e0814f9eec95
Kaan <ksonmezoz@outlook.com>
2019-07-31 00:38:29 +0300

    Intial commit

---
+++ src/main/java/com/kaansonmezoz/rest/template/playground/RestTemplatePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.kaansonmezoz.rest.template.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplatePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplatePlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaansonmezoz/rest/template/playground/RestTemplatePlaygroundApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kaansonmezoz.rest.template.playground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestTemplatePlaygroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
